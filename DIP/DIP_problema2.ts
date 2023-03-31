95d8285c91b55d2417d834ee3c6cf35906e1a955
Matheus Lino <matheuslino86@gmail.com.br>
2023-03-31 16:00:56 -0400

    feat: :sparkles: Dependency inversion principle

---
+++ DIP/DIP_problema2.ts
@@ -0,0 +1,22 @@
+// Dependency inversion principle
+// A classe Movie não deveria depender da classe Drama Category, logo está incorreto
+// class DramaCategory{
+
+// }
+
+// class Movie{
+//     private _name
+//     private _category
+
+//     public get name(){
+//         return this._name
+//     }
+
+//     public set name(name){
+//         this._name = name
+//     }
+
+//     public get category(){
+//         return new DramaCategory()
+//     }
+// }
